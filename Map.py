65cf61cdfd8c52c6d296900284a3fb45ab91d815
tim10banks <lucatrd10@gmail.com>
2018-02-13 22:03:21 +0100

    perso debut

--- Map.py
+++ Map.py
@@ -22,3 +22,17 @@ class Map:
 
         def setSpawn(self, spawnCoords):
                 self.spawnCoords = spawnCoords
+
+        class perso:
+                def_init_(fenetre,image,width,height):
+                        self.fenetre=fenetre
+                        self.image=image
+                        self.coords=width,height
+                
+                def charge(self):
+                        self.image=pygame.image.load("Resources/Persos/"+ self.image+".png")
+                        self.fenetre.blit(self.image, (coords))
+
+                
+                        
+
