fa9d7fb4861d5ddd1d49b49af642881d9167fb61
tim10banks <lucatrd10@gmail.com>
2018-03-05 22:12:59 +0100

    classe map + perso

--- utilities.py
+++ utilities.py
@@ -25,13 +25,26 @@ class Map:
 
 class Perso:
         def__init__(self,fenetre,image,coords):
-                        self.fenetre=fenetre
-                        self.image=image
-                        self.coords=coords
+                self.fenetre=fenetre
+                self.image=image
+                self.coords=self.get_rect()
                 
         def charge(self):
                 self.image=pygame.image.load("Resources/Persos/"+ self.image+".png")
                 self.fenetre.blit(self.image, (self.coords))
+
+        def haut(self,coords):
+                self.coords.move(3,0)
+        
+        def bas(self,coords):
+                self.coords.move(-3,0)
+
+        def droite(self,coords):
+                self.coords.move(0,3)
+
+        def gauche(self,coords):
+                self.coords.move(0,0-3)
+
 
 
                 
